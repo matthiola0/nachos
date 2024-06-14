16355797422da3df1ba640193b4fdaeb519f3817
matthiola0 <abc35100762@gmail.com>
2024-06-14 16:40:11 +0800

    Initial commit

---
+++ threads/alarm.cc
@@ -0,0 +1,88 @@
+// alarm.cc
+//	Routines to use a hardware timer device to provide a
+//	software alarm clock.  For now, we just provide time-slicing.
+//
+//	Not completely implemented.
+//
+// Copyright (c) 1992-1996 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#include "copyright.h"
+#include "alarm.h"
+#include "main.h"
+
+//----------------------------------------------------------------------
+// Alarm::Alarm
+//      Initialize a software alarm clock.  Start up a timer device
+//
+//      "doRandom" -- if true, arrange for the hardware interrupts to 
+//		occur at random, instead of fixed, intervals.
+//----------------------------------------------------------------------
+
+Alarm::Alarm(bool doRandom)
+{
+    timer = new Timer(doRandom, this);
+}
+
+//----------------------------------------------------------------------
+// Alarm::CallBack
+//	Software interrupt handler for the timer device. The timer device is
+//	set up to interrupt the CPU periodically (once every TimerTicks).
+//	This routine is called each time there is a timer interrupt,
+//	with interrupts disabled.
+//
+//	Note that instead of calling Yield() directly (which would
+//	suspend the interrupt handler, not the interrupted thread
+//	which is what we wanted to context switch), we set a flag
+//	so that once the interrupt handler is done, it will appear as 
+//	if the interrupted thread called Yield at the point it is 
+//	was interrupted.
+//
+//	For now, just provide time-slicing.  Only need to time slice 
+//      if we're currently running something (in other words, not idle).
+//	Also, to keep from looping forever, we check if there's
+//	nothing on the ready list, and there are no other pending
+//	interrupts.  In this case, we can safely halt.
+//----------------------------------------------------------------------
+
+void 
+Alarm::CallBack() 
+{
+    Interrupt *interrupt = kernel->interrupt;
+    MachineStatus status = interrupt->getStatus();
+    
+
+    //<TODO>
+    int currentTime = kernel->stats->totalTicks;
+    // In each 100 ticks,     
+
+    // 1. Update Priority
+    if (currentTime % 100 == 0) {
+        kernel->scheduler->UpdatePriority();
+    }
+
+    // 2. Update RunTime & RRTime
+    Thread *currentThread = kernel->currentThread;
+    currentThread->SetRunTime(currentThread->GetRunTime() + 1);
+    currentThread->SetRRTime(currentThread->GetRRTime() + 1);
+
+    // 3. Check Round Robin
+    if (currentThread->GetRRTime() >= 400) {
+        currentThread->SetRRTime(0);
+        if (status == SystemMode) {
+            kernel->interrupt->YieldOnReturn();
+        }
+    }
+
+    //<TODO>
+    
+    if (status == IdleMode) {    // is it time to quit?
+        if (!interrupt->AnyFutureInterrupts()) {
+            timer->Disable(); // turn off the timer
+        }
+    } else {         // there's someone to preempt
+       interrupt->YieldOnReturn();
+    }
+}
+
---
+++ threads/scheduler.cc
@@ -0,0 +1,340 @@
+// scheduler.cc 
+//	Routines to choose the next thread to run, and to dispatch to
+//	that thread.
+//
+// 	These routines assume that interrupts are already disabled.
+//	If interrupts are disabled, we can assume mutual exclusion
+//	(since we are on a uniprocessor).
+//
+// 	NOTE: We can't use Locks to provide mutual exclusion here, since
+// 	if we needed to wait for a lock, and the lock was busy, we would 
+//	end up calling FindNextToRun(), and that would put us in an 
+//	infinite loop.
+//
+// 	Very simple implementation -- no priorities, straight FIFO.
+//	Might need to be improved in later assignments.
+//
+// Copyright (c) 1992-1996 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#include "copyright.h"
+#include "debug.h"
+#include "scheduler.h"
+#include "main.h"
+
+#define AGING_THRESHOLD 400
+
+//----------------------------------------------------------------------
+// Scheduler::Scheduler
+// 	Initialize the list of ready but not running threads.
+//	Initially, no ready threads.
+//----------------------------------------------------------------------
+
+
+
+//<TODO>
+// Declare sorting rule of SortedList for L1 & L2 ReadyQueue
+// Hint: Funtion Type should be "static int"
+//<TODO>
+
+Scheduler::Scheduler()
+{
+//	schedulerType = type;
+    // readyList = new List<Thread *>; 
+    //<TODO>
+    // Initialize L1, L2, L3 ReadyQueue
+    //<TODO>
+    L1ReadyQueue = new SortedList<Thread *>(L1Comparison);
+    L2ReadyQueue = new SortedList<Thread *>(L2Comparison);
+    L3ReadyQueue = new List<Thread* >();
+	toBeDestroyed = NULL;
+} 
+
+//----------------------------------------------------------------------
+// Scheduler::~Scheduler
+// 	De-allocate the list of ready threads.
+//----------------------------------------------------------------------
+
+Scheduler::~Scheduler()
+{ 
+    //<TODO>
+    // Remove L1, L2, L3 ReadyQueue
+    //<TODO>
+    // delete readyList; 
+    delete L1ReadyQueue;
+    delete L2ReadyQueue;
+    delete L3ReadyQueue;
+} 
+
+//----------------------------------------------------------------------
+// Scheduler::ReadyToRun
+// 	Mark a thread as ready, but not running.
+//	Put it on the ready list, for later scheduling onto the CPU.
+//
+//	"thread" is the thread to be put on the ready list.
+//----------------------------------------------------------------------
+
+void
+Scheduler::ReadyToRun (Thread *thread)
+{
+    ASSERT(kernel->interrupt->getLevel() == IntOff);
+    // DEBUG(dbgThread, "Putting thread on ready list: " << thread->getName());
+
+    Statistics* stats = kernel->stats;
+    //<TODO>
+    // According to priority of Thread, put them into corresponding ReadyQueue.
+    // After inserting Thread into ReadyQueue, don't forget to reset some values.
+    // Hint: L1 ReadyQueue is preemptive SRTN(Shortest Remaining Time Next).
+    // When putting a new thread into L1 ReadyQueue, you need to check whether preemption or not.
+    //<TODO>
+    // readyList->Append(thread);
+    if (thread->GetPriority() >= 100) {
+        L1ReadyQueue->Insert(thread);
+        Thread *currentThread = kernel->currentThread;
+        if (currentThread->GetRemainingBurstTime() < thread->GetRemainingBurstTime())
+            kernel->currentThread->Yield();
+
+    } else if (thread->GetPriority() >= 50) {
+        L2ReadyQueue->Insert(thread);
+    } else {
+        L3ReadyQueue->Append(thread);
+    }
+
+    // DEBUG('z', "[InsertToQueue] Tick [" << stats->totalTicks << "]: Thread [" 
+    //       << thread->getID() << "] is inserted into queue L[" 
+    //       << thread->GetQueueLevel() << "]");
+}
+
+//----------------------------------------------------------------------
+// Scheduler::FindNextToRun
+// 	Return the next thread to be scheduled onto the CPU.
+//	If there are no ready threads, return NULL.
+// Side effect:
+//	Thread is removed from the ready list.
+//----------------------------------------------------------------------
+
+Thread *
+Scheduler::FindNextToRun ()
+{
+    ASSERT(kernel->interrupt->getLevel() == IntOff);
+
+    /*if (readyList->IsEmpty()) {
+    return NULL;
+    } else {
+        return readyList->RemoveFront();
+    }*/
+
+    //<TODO>
+    // a.k.a. Find Next (Thread in ReadyQueue) to Run
+    //<TODO>
+    if (!L1ReadyQueue->IsEmpty()) {
+        return L1ReadyQueue->RemoveFront();
+    } else if (!L2ReadyQueue->IsEmpty()) {
+        return L2ReadyQueue->RemoveFront();
+    } else if (!L3ReadyQueue->IsEmpty()) {
+        return L3ReadyQueue->RemoveFront();
+    } else {
+        return NULL;
+    }
+}
+
+//----------------------------------------------------------------------
+// Scheduler::Run
+// 	Dispatch the CPU to nextThread.  Save the state of the old thread,
+//	and load the state of the new thread, by calling the machine
+//	dependent context switch routine, SWITCH.
+//
+//      Note: we assume the state of the previously running thread has
+//	already been changed from running to blocked or ready (depending).
+// Side effect:
+//	The global variable kernel->currentThread becomes nextThread.
+//
+//	"nextThread" is the thread to be put into the CPU.
+//	"finishing" is set if the current thread is to be deleted
+//		once we're no longer running on its stack
+//		(when the next thread starts running)
+//----------------------------------------------------------------------
+
+void
+Scheduler::Run (Thread *nextThread, bool finishing)
+{
+    Thread *oldThread = kernel->currentThread;
+ 
+//	cout << "Current Thread" <<oldThread->getName() << "    Next Thread"<<nextThread->getName()<<endl;
+   
+    ASSERT(kernel->interrupt->getLevel() == IntOff);
+
+    if (finishing) {	// mark that we need to delete current thread
+         ASSERT(toBeDestroyed == NULL);
+	     toBeDestroyed = oldThread;
+    }
+   
+#ifdef USER_PROGRAM			// ignore until running user programs 
+    if (oldThread->space != NULL) {	// if this thread is a user program,
+
+        oldThread->SaveUserState(); 	// save the user's CPU registers
+	    oldThread->space->SaveState();
+    }
+#endif
+    
+    oldThread->CheckOverflow();		    // check if the old thread
+					    // had an undetected stack overflow
+
+    kernel->currentThread = nextThread;  // switch to the next thread
+    nextThread->setStatus(RUNNING);      // nextThread is now running
+    
+    DEBUG(dbgThread, "Switching from: " << oldThread->getName() << " to: " << nextThread->getName());
+    
+    // This is a machine-dependent assembly language routine defined 
+    // in switch.s.  You may have to think
+    // a bit to figure out what happens after this, both from the point
+    // of view of the thread and from the perspective of the "outside world".
+
+    cout << "Switching from: " << oldThread->getID() << " to: " << nextThread->getID() << endl;
+    SWITCH(oldThread, nextThread);
+
+    // we're back, running oldThread
+      
+    // interrupts are off when we return from switch!
+    ASSERT(kernel->interrupt->getLevel() == IntOff);
+
+    DEBUG(dbgThread, "Now in thread: " << kernel->currentThread->getID());
+
+    CheckToBeDestroyed();		// check if thread we were running
+					// before this one has finished
+					// and needs to be cleaned up
+    
+#ifdef USER_PROGRAM
+    if (oldThread->space != NULL) {	    // if there is an address space
+        oldThread->RestoreUserState();     // to restore, do it.
+	    oldThread->space->RestoreState();
+    }
+#endif
+}
+
+//----------------------------------------------------------------------
+// Scheduler::CheckToBeDestroyed
+// 	If the old thread gave up the processor because it was finishing,
+// 	we need to delete its carcass.  Note we cannot delete the thread
+// 	before now (for example, in Thread::Finish()), because up to this
+// 	point, we were still running on the old thread's stack!
+//----------------------------------------------------------------------
+
+void
+Scheduler::CheckToBeDestroyed()
+{
+    if (toBeDestroyed != NULL) {
+        DEBUG(dbgThread, "toBeDestroyed->getID(): " << toBeDestroyed->getID());
+        delete toBeDestroyed;
+	    toBeDestroyed = NULL;
+    }
+}
+ 
+//----------------------------------------------------------------------
+// Scheduler::Print
+// 	Print the scheduler state -- in other words, the contents of
+//	the ready list.  For debugging.
+//----------------------------------------------------------------------
+void
+Scheduler::Print()
+{
+    cout << "Ready list contents:\n";
+    // readyList->Apply(ThreadPrint);
+    L1ReadyQueue->Apply(ThreadPrint);
+    L2ReadyQueue->Apply(ThreadPrint);
+    L3ReadyQueue->Apply(ThreadPrint);
+}
+
+// <TODO>
+
+// Function 1. Function definition of sorting rule of L1 ReadyQueue
+
+// Function 2. Function definition of sorting rule of L2 ReadyQueue
+
+// Function 3. Scheduler::UpdatePriority()
+// Hint:
+// 1. ListIterator can help.
+// 2. Update WaitTime and priority in Aging situations
+// 3. After aging, Thread may insert to different ReadyQueue
+
+static int L1Comparison(Thread *a, Thread *b)
+{
+    if (a->GetRemainingBurstTime() < b->GetRemainingBurstTime())
+        return -1;
+    else if (a->GetRemainingBurstTime() > b->GetRemainingBurstTime())
+        return 1;
+    else
+        return 0;
+}
+
+static int L2Comparison(Thread *a, Thread *b)
+{
+    if (a->getID() < b->getID())
+        return -1;
+    else if (a->getID() > b->getID())
+        return 1;
+    else
+        return 0;
+}
+
+void 
+Scheduler::UpdatePriority()
+{
+    ListIterator<Thread *> *iter;
+    // L1
+    for (iter = new ListIterator<Thread *>(L1ReadyQueue); !iter->IsDone(); iter->Next()) {
+        Thread *thread = iter->Item();
+        thread->SetWaitTime(thread->GetWaitTime() + 100);
+
+        if (thread->GetWaitTime() > AGING_THRESHOLD) {
+            int oldPriority = thread->GetPriority();
+            thread->SetPriority(oldPriority + 10);
+            DEBUG('z', "[UpdatePriority] Tick [" << kernel->stats->totalTicks << "]: Thread ["
+                << thread->getID() << "] changes its priority from ["
+                << oldPriority << "] to [" << thread->GetPriority() << "]");
+        }
+    }
+    delete iter;
+
+    // L2 
+    for (iter = new ListIterator<Thread *>(L2ReadyQueue); !iter->IsDone(); iter->Next()) {
+        Thread *thread = iter->Item();
+        thread->SetWaitTime(thread->GetWaitTime() + 100);
+
+        if (thread->GetWaitTime() > AGING_THRESHOLD) {
+            int oldPriority = thread->GetPriority();
+            thread->SetPriority(oldPriority + 10);
+            if (thread->GetPriority() >= 100) {
+                L2ReadyQueue->Remove(thread);
+                ReadyToRun(thread);
+            }
+            
+            DEBUG('z', "[UpdatePriority] Tick [" << kernel->stats->totalTicks << "]: Thread ["
+                << thread->getID() << "] changes its priority from ["
+                << oldPriority << "] to [" << thread->GetPriority() << "]");
+        }
+    }
+    delete iter;
+
+    // 3
+    for (iter = new ListIterator<Thread *>(L3ReadyQueue); !iter->IsDone(); iter->Next()) {
+        Thread *thread = iter->Item();
+        thread->SetWaitTime(thread->GetWaitTime() + 100);
+
+        if (thread->GetWaitTime() > AGING_THRESHOLD) {
+            int oldPriority = thread->GetPriority();
+            thread->SetPriority(oldPriority + 10);
+            if (thread->GetPriority() >= 50) {
+                L3ReadyQueue->Remove(thread);
+                ReadyToRun(thread);
+            }
+            DEBUG('z', "[UpdatePriority] Tick [" << kernel->stats->totalTicks << "]: Thread ["
+                << thread->getID() << "] changes its priority from ["
+                << oldPriority << "] to [" << thread->GetPriority() << "]");
+        }
+    }
+    delete iter;
+}
+
+// <TODO>
